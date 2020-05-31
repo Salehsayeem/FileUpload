38db522652c51b4bae2f49d91c4a69a482ca63ed
Saleh Sayeem <16103179@iubat.edu>
2020-05-31 16:00:22 +0600

    Initial

---
+++ FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                string _FileName = Path.GetFileName(file.FileName);
+                string _path = Path.Combine(Server.MapPath("~/Files"),_FileName);
+                file.SaveAs(_path);
+            }
+            ViewBag.message = "File Uploaded Successfully";
+            return View();
+        }
+    }
+}
